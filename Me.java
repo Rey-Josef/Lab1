fef83cbe15a14c9c42a423f227a87fedecdc73bc
Rey-Josef Kerbaj”git config --global user.name Rey-Josef <Reyjosef.kerbaj@lau.edu>
2023-01-23 22:09:57 +0400

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me{
 	public static void main (String[] args){
 		System.out.println("Rey-Josef Kerbaj");
 		System.out.println("Mechatronics Engineering");
+		System.out.println("1st Year");
 	}
 }
