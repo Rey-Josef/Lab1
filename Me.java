ff43ac326b0e8c8c28c44b89cc211c603f86bb62
Rey-Josef Kerbaj”git config --global user.name Rey-Josef <Reyjosef.kerbaj@lau.edu>
2023-01-23 22:18:45 +0400

    Revert "My year of study added"
    This reverts commit fef83cbe15a14c9c42a423f227a87fedecdc73bc.

--- Me.java
+++ Me.java
@@ -2,6 +2,5 @@ public class Me{
 	public static void main (String[] args){
 		System.out.println("Rey-Josef Kerbaj");
 		System.out.println("Mechatronics Engineering");
-		System.out.println("1st Year");
 	}
 }
